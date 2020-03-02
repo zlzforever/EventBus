15d190c1cf78a6829c1647469fe14725fff10dd2
Lewis Zou <zlzforever@163.com>
2020-03-02 08:30:39 +0800

    fix Event is not interit from IEvent

--- src/EventBus/Event.cs
+++ src/EventBus/Event.cs
@@ -2,37 +2,37 @@ using System;
 
 namespace EventBus
 {
-    public abstract class Event
-    {
-        /// <summary>
-        /// 事件源标识
-        /// </summary>
-        public Guid EventId { get; set; }
+	public abstract class Event : IEvent
+	{
+		/// <summary>
+		/// 事件源标识
+		/// </summary>
+		public Guid EventId { get; set; }
 
-        /// <summary>
-        /// 事件发生时间
-        /// </summary>
-        public DateTimeOffset EventTime { get; set; }
+		/// <summary>
+		/// 事件发生时间
+		/// </summary>
+		public DateTimeOffset EventTime { get; set; }
 
-        /// <summary>
-        /// 触发事件的对象
-        /// </summary>
-        public object EventSource { get; set; }
+		/// <summary>
+		/// 触发事件的对象
+		/// </summary>
+		public object EventSource { get; set; }
 
-        protected Event()
-        {
-            EventId = Guid.NewGuid();
-            EventTime = DateTimeOffset.UtcNow;
-        }
+		protected Event()
+		{
+			EventId = Guid.NewGuid();
+			EventTime = DateTimeOffset.UtcNow;
+		}
 
-        /// <summary>
-        /// 判断事件是否过期
-        /// </summary>
-        /// <param name="seconds"></param>
-        /// <returns></returns>
-        public bool IsExpired(int seconds = 30)
-        {
-            return (DateTimeOffset.Now - EventTime).TotalSeconds > seconds;
-        }
-    }
+		/// <summary>
+		/// 判断事件是否过期
+		/// </summary>
+		/// <param name="seconds"></param>
+		/// <returns></returns>
+		public bool IsExpired(int seconds = 30)
+		{
+			return (DateTimeOffset.Now - EventTime).TotalSeconds > seconds;
+		}
+	}
 }
